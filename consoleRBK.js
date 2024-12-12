5665c564bfdf9d88bb471189df6f26ff89a9be77
swnevin <swnevin@kihjelp.no>
2024-12-12 09:45:06 +0100

    Create consoleRBK.js

---
+++ consoleRBK.js
@@ -0,0 +1,28 @@
+ let proactiveMessage = "Hei, jeg hjelper deg gjerne!👋";
+    let widgetStyleSheet = "https://swnevin.github.io/norwaycup_assets/styleRBK.css";
+    let FormExtension = "https://swnevin.github.io/norwaycup_assets/extensions.js";
+
+    (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '6759d3c30d7aa8a58f2d2a56' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          allowDangerousHTML: true,
+          assistant: {
+            extensions: FormExtension,
+            stylesheet: widgetStyleSheet
+          }
+        }).then(() => {
+          console.log('Voiceflow widget loaded successfully');
+          window.voiceflow.chat.proactive.clear();
+          window.voiceflow.chat.proactive.push(
+            { type: 'text', payload: { message: proactiveMessage } }
+          );
+        }).catch((err) => console.error('Widget load failed:', err));
+      };
+      v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
+      v.type = "text/javascript";
+      s.parentNode.insertBefore(v, s);
+    })(document, 'script');
